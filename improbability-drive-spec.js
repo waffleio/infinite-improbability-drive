b3202ad4202407478ecb53c62c70a8bee6aee536
Andrew Homeyer <homeyer@gmail.com>
2018-01-29 14:49:05 -0700

    fix test

--- improbability-drive-spec.js
+++ improbability-drive-spec.js
@@ -5,7 +5,7 @@ const ImprobabilityDrive = require('./improbability-drive');
 describe('ImprobabilityDrive', function(){
 
   it('rescues Arthur Dent and Ford Prefect from open space, if probability under two to the power of 276,709 to one against', function(){
-    let rescued = ProbabilityDrive.calculate(Math.pow(2, 276709)-1)
+    let rescued = ImprobabilityDrive.calculate(Math.pow(2, 276709)-1)
     expect(rescued).to.be.true
   });
 
